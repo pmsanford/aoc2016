9016d7055b5f5adde30c62c7c5ff0a2aa651e544
Paul Sanford <me@paulsanford.net>
2016-12-02 22:00:09 -0600

    Track coords

--- day1/day1.go
+++ day1/day1.go
@@ -8,13 +8,25 @@ import (
 )
 
 const (
-	L uint = iota
-	R
+	L = -1
+	R = 1
+)
+
+const (
+	N int = 0
+	E int = 1
+	S int = 2
+	W int = 3
 )
 
 type Direction struct {
-	turn uint
+	turn int
 	dist uint
+}
+
+type Coords struct {
+	x int
+	y int
 }
 
 func (dir Direction) String() string {
@@ -32,27 +44,64 @@ func check(e error) {
 }
 
 func parse_input(filename string, ch chan Direction) {
+	defer close(ch)
 	dat, err := ioutil.ReadFile(filename)
 	check(err)
 	istr := string(dat)
 	dirs := strings.Split(istr, ", ")
 	for i := range dirs {
-		turnid := dirs[i][0]
+		dirstr := strings.Trim(dirs[i], "\r\n ")
+		if len(dirstr) < 2 {
+			continue
+		}
+		turnid := dirstr[0]
 		turn := R
 		if turnid == 76 {
 			turn = L
 		}
-		count, _ := strconv.ParseInt(dirs[i][1:], 10, 64)
+		count, err := strconv.ParseInt(dirstr[1:], 10, 64)
+		check(err)
 		direction := Direction{turn, uint(count)}
 		ch <- direction
 	}
-	close(ch)
+}
+
+func make_turn(facing, turn_dir int) int {
+	facing = facing + turn_dir
+	if facing == -1 {
+		facing = 3
+	}
+	if facing == 4 {
+		facing = 0
+	}
+	return facing
+}
+
+func make_move(coords *Coords, facing int, dist uint) {
+	switch facing {
+	case N:
+		coords.x += int(dist)
+		break
+	case E:
+		coords.y += int(dist)
+		break
+	case S:
+		coords.x -= int(dist)
+		break
+	case W:
+		coords.y -= int(dist)
+		break
+	}
 }
 
 func main() {
 	ch := make(chan Direction)
 	go parse_input("input.txt", ch)
+	facing := N
+	coords := Coords{0, 0}
 	for val := range ch {
-		fmt.Println(val)
+		facing = make_turn(facing, val.turn)
+		make_move(&coords, facing, val.dist)
 	}
+	fmt.Println(coords)
 }
